28218f47b28650c9750b16fb59eb0a6aae40a685
ogultacho <67529738+ogultacho@users.noreply.github.com>
2020-08-07 12:40:06 -0400

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -4,5 +4,7 @@ public class GitFun {
     public static void main(String[] args) {
 
         System.out.println("Everything id fun");
+        
+        //we added this line from github, so nice
     }
 }
